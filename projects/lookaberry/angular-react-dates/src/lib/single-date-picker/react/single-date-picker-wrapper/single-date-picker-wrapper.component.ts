7445e396155aa4cac6bb6904fcf7e281d2a85ed1
NazimMertBilgi_KaliLinux <nmbjobs@outlook.com.tr>
2020-02-23 13:58:24 +0300

    development..

---
+++ projects/lookaberry/angular-react-dates/src/lib/single-date-picker/react/single-date-picker-wrapper/single-date-picker-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-single-date-picker-wrapper',
+  templateUrl: './single-date-picker-wrapper.component.html',
+  styleUrls: ['./single-date-picker-wrapper.component.css']
+})
+export class SingleDatePickerWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
