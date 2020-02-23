066653f063fe32096d0105f2f8d3cb2a1ae70692
NazimMertBilgi_KaliLinux <nmbjobs@outlook.com.tr>
2020-02-23 13:40:30 +0300

    single-date-picker added.

---
+++ projects/lookaberry/angular-react-dates/src/lib/single-date-picker/single-date-picker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-single-date-picker',
+  templateUrl: './single-date-picker.component.html',
+  styleUrls: ['./single-date-picker.component.css']
+})
+export class SingleDatePickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
